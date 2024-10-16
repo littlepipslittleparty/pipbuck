871aff5dca4ff6dc203cd797809954c59da01e04
HalavicH <HalavicH@gmail.com>
2024-10-16 22:45:12 +0200

    [vue3-vite-ts-pinia] Extract helper functions from the main component

---
+++ src/lib/app-helper.js
@@ -0,0 +1,77 @@
+// src/app-helper.js
+import Shake from 'shake.js';
+
+export function mounted() {
+    // watch colorFront and colorBack to update the <body> element styles.
+    this.$watch(
+        'colorFront',
+        // eslint-disable-next-line prefer-arrow-callback
+        function watchColorFront(newColor) {
+            document.body.style.color = newColor;
+        },
+        {immediate: true},
+    );
+    /* this.$watch(
+      'colorBack',
+      // eslint-disable-next-line prefer-arrow-callback
+      function watchColorBack(newColor) {
+        document.body.style.backgroundColor = newColor;
+      },
+      { immediate: true },
+    ); */
+    // event listeners
+    this.shake_instance = new Shake({
+        threshold: 15, // optional shake strength threshold
+        timeout: 1000, // optional, determines the frequency of event generation
+    });
+    this.shake_instance.start();
+    window.addEventListener('orientationchange', this.updateHardwareButtonPosition.bind(this));
+    window.addEventListener('keyup', (event) => {
+        // If a special navigation key was pressed...
+        /*
+        h:         Display Help.
+        q|tab:     Quit.
+        a|s:       STATS button.
+        b|i:       ITEMS button.
+        c|d:       DATA  button.
+        1-5:       Select Tabs.
+        +|up:      Scroll up.
+        -|down:    Scroll down.
+        8-0: Change colors.
+         */
+        // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code/code_values
+        if (event.key === 'a' || event.key === 's') {
+            this.$router.push({name: 'Stats'});
+        } else if (event.key === 'b' || event.key === 'i') {
+            this.$router.push({name: 'Items'});
+        } else if (event.key === 'c' || event.key === 'd') {
+            this.$router.push({name: 'Data'});
+        } else if (event.key === '.' || event.key === 'Escape') {
+            this.$router.push({name: 'Boot'});
+        } else if (event.key === ',' || event.key === 'p') {
+            this.$router.push({name: 'Settings'});
+        } else if (event.key === 'm') {
+            this.$router.push({name: 'Local Map'});
+        } else if (event.key === 'h') {
+            this.$router.push({name: 'About'});
+        } else if (event.key === 'r') {
+            this.$router.push({name: 'Radio'});
+        } else if (event.key === '+' || event.key === 'ArrowUp') {
+            this.$broadcast('navigate-UP');
+        } else if (event.key === '-' || event.key === 'ArrowDown') {
+            this.$broadcast('navigate-DOWN');
+        } else if (event.key === '<' || event.key === 'ArrowLeft') {
+            this.$broadcast('navigate-LEFT');
+        } else if (event.key === '>' || event.key === 'ArrowRight') {
+            this.$broadcast('navigate-RIGHT');
+        }
+    });
+}
+
+export function beforeDestroy() {
+    if (this.scroll_prevent !== null) {
+        this.shake_instance.stop();
+        this.shake_instance = null;
+    }
+    window.removeEventListener('orientationchange', this.updateHardwareButtonPosition.bind(this));
+}
