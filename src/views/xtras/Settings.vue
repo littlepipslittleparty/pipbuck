8190a7d20cca34200919c6f04ee80892e0fc9198
HalavicH <HalavicH@gmail.com>
2024-10-16 22:14:39 +0200

    [vue3-vite-ts-pinia] refactoring: Rewrite Settings to vue3

--- src/views/xtras/Settings.vue
+++ src/views/xtras/Settings.vue
@@ -4,7 +4,7 @@
       <a href="/">Reload</a>
     </div>
     <div>
-      <router-link :to="{ name: 'Boot' }">Turn Off</router-link>
+      <RouterLink :to="{ name: 'Boot' }">Turn Off</RouterLink>
     </div>
     <div>
       <a @click="toggleFullscreen" :class="{ active: screenfull.isFullscreen }">Fullscreen</a>
@@ -18,133 +18,136 @@
       <label>Color</label>
       <div v-for="(row, i) in colors" :key="i">
         <a
-          v-for="color in row" :key="color.name"
-          @click="setColor(color)" class="active"
+            v-for="color in row" :key="color.name"
+            @click="setColor(color)" class="active"
         >{{ color.name }}</a>
-         <!-- :style="{ borderColor: color.foreground, color: color.foreground,
-        backgroundColor: color.background }"-->
+        <!-- :style="{ borderColor: color.foreground, color: color.foreground,
+       backgroundColor: color.background }"-->
       </div>
     </div>
     <div>
-        <stable-colt :hair="hair" :back="back"/>
+      <StableColt :hair="hair" :back="back"/>
     </div>
   </div>
 </template>
 
 <script>
+import {computed, reactive} from 'vue';
+import {useStore} from 'vuex';
 import * as screenfull from 'screenfull';
-import { hsl, HexToHSL } from '../../lib/colorspace';
+import {HexToHSL, hsl} from '../../lib/colorspace';
 import StableColt from '../../components/StableColt.vue';
-import { ui } from '../../sound';
+import {ui} from '../../sound';
 
 export default {
   name: 'Settings',
-  components: { StableColt },
-  data() {
-    return {
-      colors: [
-        // Original colors:
-        // https://www.reddit.com/r/Fallout/comments/40hpkr/classic_hud_colors/
-        //
-        // Converter:
-        // https;//rgb.to/hex/RRGGBB
-        [ // row 1
-          {
-            name: 'Amber',
-            foreground: hsl(37, 100, 63, '#ffb642'), // rgb(255, 182, 66)
-            background: hsl(37, 78, 14, '#402a08'),
-          },
-          {
-            name: 'Blue',
-            foreground: hsl(194, 100, 59, '#2ecfff'), // rgb(46, 207, 255)
-            background: hsl(194, 78, 14, '#083340'),
-          },
-          {
-            name: 'Green',
-            foreground: hsl(147, 100, 55, '#1aff80'), // rgb(26, 255, 128)
-            background: hsl(147, 78, 14, '#084021'),
-          },
-          {
-            name: 'White',
-            foreground: hsl(180, 100, 88, '#c0ffff'), // rgb(192, 255, 255)
-            background: hsl(180, 100, 14, '#004747'),
-          },
-        ], [
-          {
-            name: 'Alt Amber',
-            foreground: hsl(40, 85, 79, '#f7da9e'),
-            background: hsl(40, 78, 14, '#402d08'),
-          },
-          {
-            name: 'Alt Blue',
-            foreground: hsl(180, 85, 79, '#9ff5f5'),
-            background: hsl(180, 78, 14, '#084040'),
-          },
-          {
-            name: 'Alt Green',
-            foreground: hsl(145, 85, 79, '#9cf7c2'),
-            background: hsl(145, 78, 14, '#08401f'),
-          },
-          {
-            name: 'Alt White',
-            foreground: hsl(0, 0, 96, '#f5f5f5'),
-            background: hsl(0, 0, 25, '#404040'),
-          },
-        ], [
-          {
-            name: 'Alt Orange',
-            foreground: hsl(20, 100, 50),
-            background: hsl(40, 78, 14, '#402d08'),
-          },
-          {
-            name: 'Alt Red',
-            foreground: hsl(359, 85, 79, '#f79c9d'),
-            background: hsl(359, 78, 14, '#400809'),
-          },
-          {
-            name: 'Alt Violet',
-            foreground: hsl(270, 85, 79, '#ca9ff5'),
-            background: hsl(270, 78, 14, '#240840'),
-          },
-        ],
+  components: {StableColt},
+  setup() {
+    const store = useStore();
+    const colors = reactive([
+      [
+        {
+          name: 'Amber',
+          foreground: hsl(37, 100, 63, '#ffb642'),
+          background: hsl(37, 78, 14, '#402a08'),
+        },
+        {
+          name: 'Blue',
+          foreground: hsl(194, 100, 59, '#2ecfff'),
+          background: hsl(194, 78, 14, '#083340'),
+        },
+        {
+          name: 'Green',
+          foreground: hsl(147, 100, 55, '#1aff80'),
+          background: hsl(147, 78, 14, '#084021'),
+        },
+        {
+          name: 'White',
+          foreground: hsl(180, 100, 88, '#c0ffff'),
+          background: hsl(180, 100, 14, '#004747'),
+        },
       ],
-    };
-  },
-  computed: {
-    hair() {
-      return HexToHSL(this.$store.state.colorFront);
-    },
-    back() {
-      return HexToHSL(this.$store.state.colorBack);
-    },
-    screenfull() {
-      return screenfull;
-    },
-  },
-  methods: {
-    leToggleVirtualButtons(state) {
+      [
+        {
+          name: 'Alt Amber',
+          foreground: hsl(40, 85, 79, '#f7da9e'),
+          background: hsl(40, 78, 14, '#402d08'),
+        },
+        {
+          name: 'Alt Blue',
+          foreground: hsl(180, 85, 79, '#9ff5f5'),
+          background: hsl(180, 78, 14, '#084040'),
+        },
+        {
+          name: 'Alt Green',
+          foreground: hsl(145, 85, 79, '#9cf7c2'),
+          background: hsl(145, 78, 14, '#08401f'),
+        },
+        {
+          name: 'Alt White',
+          foreground: hsl(0, 0, 96, '#f5f5f5'),
+          background: hsl(0, 0, 25, '#404040'),
+        },
+      ],
+      [
+        {
+          name: 'Alt Orange',
+          foreground: hsl(20, 100, 50),
+          background: hsl(40, 78, 14, '#402d08'),
+        },
+        {
+          name: 'Alt Red',
+          foreground: hsl(359, 85, 79, '#f79c9d'),
+          background: hsl(359, 78, 14, '#400809'),
+        },
+        {
+          name: 'Alt Violet',
+          foreground: hsl(270, 85, 79, '#ca9ff5'),
+          background: hsl(270, 78, 14, '#240840'),
+        },
+      ],
+    ]);
+
+    const hair = computed(() => HexToHSL(store.state.colorFront));
+    const back = computed(() => HexToHSL(store.state.colorBack));
+    const screenfullState = computed(() => screenfull);
+
+    const leToggleVirtualButtons = (state) => {
       console.log('commiting toggleHardwareButtons state: ', state);
-      this.$store.commit('toggleHardwareButtons', state);
-    },
-    toggleFullscreen() {
+      store.commit('toggleHardwareButtons', state);
+    };
+
+    const toggleFullscreen = () => {
       ui.play(ui.sounds.nav_fullscreen);
       screenfull.toggle();
-    },
-    setColor(color) {
+    };
+
+    const setColor = (color) => {
       console.log('commiting setColorFront state: ', color);
-      this.$store.commit('setColorFront', color.foreground);
-      this.$store.commit('setColorBack', color.background);
-    },
+      store.commit('setColorFront', color.foreground);
+      store.commit('setColorBack', color.background);
+    };
+
+    return {
+      colors,
+      hair,
+      back,
+      screenfull: screenfullState,
+      leToggleVirtualButtons,
+      toggleFullscreen,
+      setColor,
+    };
   },
 };
 </script>
 
 <style scoped>
-  div {
-    margin-top: 1rem;
-  }
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-  }
+div {
+  margin-top: 1rem;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
 </style>
