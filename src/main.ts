0cda6eb14f0f33b922d881fb5c7c21abced0d7cd
HalavicH <HalavicH@gmail.com>
2024-10-17 11:08:59 +0200

    main: Refactor app initialization

--- src/main.ts
+++ src/main.ts
@@ -9,30 +9,27 @@ import store from './state';
 import {createPinia} from "pinia";
 
 const app = createApp(App);
-const pinia = createPinia()
-app.use(pinia)
-app.use(router)
-    .use(store);
-
-const metaManager = createMetaManager();
-app.use(metaManager);
-
-
-app.use(VueGtag, {
-  config: {
-    id: process.env.NODE_ENV !== 'production' ? 'UA-47744366-5' : 'UA-47744366-4',
-    params: {
-      send_page_view: true,
-    },
-    appName: 'Pipbuck v2'
-  },
-  pageTrackerScreenviewEnabled: true,
-}, router);
+app.use(createPinia())
+    .use(router)
+    .use(store)
+    .use(createMetaManager())
+    .use(VueGtag, {
+        config: {
+            id: process.env.NODE_ENV !== 'production' ? 'UA-47744366-5' : 'UA-47744366-4',
+            params: {
+                send_page_view: true,
+            },
+            appName: 'Pipbuck v2'
+        },
+        pageTrackerScreenviewEnabled: true,
+    }, router);
 app.mount('#app');
 
 if (process.env.NODE_ENV !== 'production') {
     globalThis.$vm = app;
 }
+
 if (screenfull.isEnabled) {
+    // Seems to be broken after migration to vue 3
     // screenfull.request();
 }
