38bd53ac004ffd82c5cbe11d01ea8eb7d39597ee
luckydonald <m1-mac-2024._.code@luckydonald.de>
2024-10-17 15:08:34 +0200

    vite: diffable config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), svgLoader()],
+    plugins: [
+      vue(),
+      svgLoader(),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, '/src'),
