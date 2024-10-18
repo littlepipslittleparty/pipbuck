d9a47bd4ee7b411ffc199f872ade2713a9b4d86d
HalavicH <HalavicH@gmail.com>
2024-10-18 11:20:04 +0200

    build: Parametrize devtools editor

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
 // vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 import svgLoader from 'vite-svg-loader';
+
+console.log("Using ", process.env.VITE_LAUNCH_EDITOR || code, "as the editor for vue devtools");
 
 // https://vite.dev/config/
 export default defineConfig({
     optimizeDeps: {
-      entries: [
-        // globs are supported, too.
-        './index.html',
-      ],
+        entries: [
+            './index.html',
+        ],
     },
     plugins: [
-      vue(),
-      svgLoader(),
-      vueDevTools({
-        componentInspector: true, // default
-        launchEditor: 'pycharm',
-      }),
+        vue(),
+        svgLoader(),
+        vueDevTools({
+            componentInspector: true, // default
+            launchEditor: process.env.VITE_LAUNCH_EDITOR || 'code', // Use .env file to override
+        }),
     ],
     resolve: {
         alias: {
